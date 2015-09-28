ed3c536a3f0c54b0a9a84736466b7c841cfc30ba
djadjavano <djadjavano@mail.ru>
2015-09-28 23:03:34 +0300

    update all2

--- 7_auth_1.php
+++ 7_auth_1.php
@@ -1,9 +1,24 @@
-﻿<?php
-        $link = mysql_connect('localhost', 'root', '');
+<?php
+$link = mysql_connect('localhost', 'root', '');
 if (!$link) {
     die('Ошибка соединения: ' . mysql_error());
 }
-mysql_select_db("test") or die('BD not selected');
+mysql_select_db('test') or die('BD not selected');
+
+
+$login = $_REQUEST['login'];
+$password = $_REQUEST['password'];
+$res = mysql_query('SELECT id FROM user
+WHERE login="'.$login.'" AND password="'.$password.'"');
+if (!$res) {
+die(mysql_error());
+}
+list($user_id) = mysql_fetch_array($res);
+//var_dump($user_id);
+session_start();
+$_SESSION['authorized'] = isset($user_id);
+         mysql_close($link);
+
 /*$login = mysql_real_escape_string($_POST['login']);
 $password = mysql_real_escape_string($_POST['password']);
 if ($res = mysql_query('SELECT id FROM users WHERE login="'.$login.'" AND password="'.$password.'"')) {
@@ -13,16 +28,5 @@ if ($res = mysql_query('SELECT id FROM users WHERE login="'.$login.'" AND passwo
 } else {
     echo 'Not auth...';
 }*/
-$login = $_REQUEST['login'];
-$password = $_REQUEST['password'];
-$res = mysql_query('SELECT id FROM users
-
-WHERE login="'.$login.'" AND password="'.$password.'"');
-list($user_id) = mysql_fetch_array($res);
-session_start();
-var_dump($res);
-$_SESSION['authorized'] = isset($user_id);
-
-//UPDATE users SET password='0' WHERE login='admin'
 ?>
 
