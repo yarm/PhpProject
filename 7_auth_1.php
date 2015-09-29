d8ecd1d0bc053884b0da8a9828017828543bea3e
djadjavano <djadjavano@mail.ru>
2015-09-30 00:29:56 +0300

    update all30

--- 7_auth_1.php
+++ 7_auth_1.php
@@ -11,14 +11,16 @@ $password = $_REQUEST['password'];
 $res = mysql_query('SELECT id FROM user WHERE login="'.$login.'" AND password="'.$password.'"');
 var_dump('SELECT id FROM user WHERE login="'.$login.'" AND password="'.$password.'"');
 list($user_id) = mysql_fetch_array($res);
-
- 
+$resolt=mysql_fetch_array($res);
+var_dump($resolt);
 session_start();
 $_SESSION['authorized'] = isset($user_id);
+
 if ($_SESSION) var_dump ($_SESSION);
          mysql_close($link);
 
          //login=2" UNION SELECT password AS id FROM user WHERE 1 OR "1
+         //працює навіть без використання логіна,фактично не потрібен ні логін не пароль))))
          
 
 /*$login = mysql_real_escape_string($_POST['login']);
